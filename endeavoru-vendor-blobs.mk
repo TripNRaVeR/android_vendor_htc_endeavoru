946e727b2e314e69006bb4100e39549237d44105
TripNRaVeR <noerialexeew@live.nl>
2012-12-28 16:50:29 +0100

    import 008 release jb-4.2.1

--- endeavoru-vendor-blobs.mk
+++ endeavoru-vendor-blobs.mk
@@ -17,7 +17,6 @@
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/bin/akmd:/system/bin/akmd \
-    $(LOCAL_PATH)/proprietary/bin/charging:/system/bin/charging \
     $(LOCAL_PATH)/proprietary/bin/cplc_main:/system/bin/cplc_main \
     $(LOCAL_PATH)/proprietary/bin/ewtzmud:/system/bin/ewtzmud \
     $(LOCAL_PATH)/proprietary/bin/hdmid:/system/bin/hdmid \
@@ -41,10 +40,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/lib/egl/libEGL_perfhud.so:/system/lib/egl/libEGL_perfhud.so \
     $(LOCAL_PATH)/proprietary/lib/egl/libEGL_tegra.so:/system/lib/egl/libEGL_tegra.so \
+    $(LOCAL_PATH)/proprietary/lib/egl/libEGL_tegra_impl.so:/system/lib/egl/libEGL_tegra_impl.so \
     $(LOCAL_PATH)/proprietary/lib/egl/libGLESv1_CM_perfhud.so:/system/lib/egl/libGLESv1_CM_perfhud.so \
     $(LOCAL_PATH)/proprietary/lib/egl/libGLESv1_CM_tegra.so:/system/lib/egl/libGLESv1_CM_tegra.so \
+    $(LOCAL_PATH)/proprietary/lib/egl/libGLESv1_CM_tegra_impl.so:/system/lib/egl/libGLESv1_CM_tegra_impl.so \
     $(LOCAL_PATH)/proprietary/lib/egl/libGLESv2_perfhud.so:/system/lib/egl/libGLESv2_perfhud.so \
     $(LOCAL_PATH)/proprietary/lib/egl/libGLESv2_tegra.so:/system/lib/egl/libGLESv2_tegra.so \
+    $(LOCAL_PATH)/proprietary/lib/egl/libGLESv2_tegra_impl.so:/system/lib/egl/libGLESv2_tegra_impl.so \
     $(LOCAL_PATH)/proprietary/lib/hw/audio.primary.tegra.so:/system/lib/hw/audio.primary.tegra.so \
     $(LOCAL_PATH)/proprietary/lib/hw/audio_policy.tegra.so:/system/lib/hw/audio_policy.tegra.so \
     $(LOCAL_PATH)/proprietary/lib/hw/camera.tegra.so:/system/lib/hw/camera.tegra.so \
@@ -54,7 +56,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/lib/hw/sensors.endeavoru.so:/system/lib/hw/sensors.endeavoru.so \
     $(LOCAL_PATH)/proprietary/lib/libardrv_dynamic.so:/system/lib/libardrv_dynamic.so \
     $(LOCAL_PATH)/proprietary/lib/libasound.so:/system/lib/libasound.so \
+    $(LOCAL_PATH)/proprietary/lib/libBeautyChat.so:/system/lib/libBeautyChat.so \
     $(LOCAL_PATH)/proprietary/lib/libcall_volume.so:/system/lib/libcall_volume.so \
+    $(LOCAL_PATH)/proprietary/lib/libcameraasd.so:/system/lib/libcameraasd.so \
+    $(LOCAL_PATH)/proprietary/lib/libcameraface.so:/system/lib/libcameraface.so \
+    $(LOCAL_PATH)/proprietary/lib/libcamerapp.so:/system/lib/libcamerapp.so \
     $(LOCAL_PATH)/proprietary/lib/libcgdrv.so:/system/lib/libcgdrv.so \
     $(LOCAL_PATH)/proprietary/lib/libhtc_dis.so:/system/lib/libhtc_dis.so \
     $(LOCAL_PATH)/proprietary/lib/libhtc_ril.so:/system/lib/libhtc_ril.so \
@@ -68,14 +74,20 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/lib/libmcphalgps.so:/system/lib/libmcphalgps.so \
     $(LOCAL_PATH)/proprietary/lib/libnvapputil.so:/system/lib/libnvapputil.so \
     $(LOCAL_PATH)/proprietary/lib/libnvasfparserhal.so:/system/lib/libnvasfparserhal.so \
+    $(LOCAL_PATH)/proprietary/lib/libnvaudioservice.so:/system/lib/libnvaudioservice.so \
     $(LOCAL_PATH)/proprietary/lib/libnvaviparserhal.so:/system/lib/libnvaviparserhal.so \
     $(LOCAL_PATH)/proprietary/lib/libnvavp.so:/system/lib/libnvavp.so \
+    $(LOCAL_PATH)/proprietary/lib/libnvcam_imageencoder.so:/system/lib/libnvcam_imageencoder.so \
+    $(LOCAL_PATH)/proprietary/lib/libnvcameracallbacks.so:/system/lib/libnvcameracallbacks.so \
+    $(LOCAL_PATH)/proprietary/lib/libnvcamerahdr.so:/system/lib/libnvcamerahdr.so \
+    $(LOCAL_PATH)/proprietary/lib/libnvcap.so:/system/lib/libnvcap.so \
+    $(LOCAL_PATH)/proprietary/lib/libnvcap_video.so:/system/lib/libnvcap_video.so \
+    $(LOCAL_PATH)/proprietary/lib/libnvcapclk.so:/system/lib/libnvcapclk.so \
     $(LOCAL_PATH)/proprietary/lib/libnvcontrol_jni.so:/system/lib/libnvcontrol_jni.so \
-    $(LOCAL_PATH)/proprietary/lib/libnvcpud_client.so:/system/lib/libnvcpud_client.so \
     $(LOCAL_PATH)/proprietary/lib/libnvcpud.so:/system/lib/libnvcpud.so \
     $(LOCAL_PATH)/proprietary/lib/libnvddk_2d.so:/system/lib/libnvddk_2d.so \
     $(LOCAL_PATH)/proprietary/lib/libnvddk_2d_v2.so:/system/lib/libnvddk_2d_v2.so \
-    $(LOCAL_PATH)/proprietary/lib/libnvdispmgr_d.so:/system/lib/libnvdispmgr_d.so \
+    $(LOCAL_PATH)/proprietary/lib/libnvglsi.so:/system/lib/libnvglsi.so \
     $(LOCAL_PATH)/proprietary/lib/libnvmm_asfparser.so:/system/lib/libnvmm_asfparser.so \
     $(LOCAL_PATH)/proprietary/lib/libnvmm_audio.so:/system/lib/libnvmm_audio.so \
     $(LOCAL_PATH)/proprietary/lib/libnvmm_aviparser.so:/system/lib/libnvmm_aviparser.so \
@@ -89,7 +101,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/lib/libnvmmlite_utils.so:/system/lib/libnvmmlite_utils.so \
     $(LOCAL_PATH)/proprietary/lib/libnvmmlite_video.so:/system/lib/libnvmmlite_video.so \
     $(LOCAL_PATH)/proprietary/lib/libnvmm_manager.so:/system/lib/libnvmm_manager.so \
-    $(LOCAL_PATH)/proprietary/lib/libnvmm_misc.so:/system/lib/libnvmm_misc.so \
     $(LOCAL_PATH)/proprietary/lib/libnvmm_msaudio.so:/system/lib/libnvmm_msaudio.so \
     $(LOCAL_PATH)/proprietary/lib/libnvmm_parser.so:/system/lib/libnvmm_parser.so \
     $(LOCAL_PATH)/proprietary/lib/libnvmm_service.so:/system/lib/libnvmm_service.so \
@@ -97,15 +108,15 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/lib/libnvmm_utils.so:/system/lib/libnvmm_utils.so \
     $(LOCAL_PATH)/proprietary/lib/libnvmm_vc1_video.so:/system/lib/libnvmm_vc1_video.so \
     $(LOCAL_PATH)/proprietary/lib/libnvmm_video.so:/system/lib/libnvmm_video.so \
-    $(LOCAL_PATH)/proprietary/lib/libnvmm_vp6_video.so:/system/lib/libnvmm_vp6_video.so \
     $(LOCAL_PATH)/proprietary/lib/libnvmm_writer.so:/system/lib/libnvmm_writer.so \
     $(LOCAL_PATH)/proprietary/lib/libnvodm_dtvtuner.so:/system/lib/libnvodm_dtvtuner.so \
     $(LOCAL_PATH)/proprietary/lib/libnvodm_hdmi.so:/system/lib/libnvodm_hdmi.so \
     $(LOCAL_PATH)/proprietary/lib/libnvodm_imager.so:/system/lib/libnvodm_imager.so \
     $(LOCAL_PATH)/proprietary/lib/libnvodm_misc.so:/system/lib/libnvodm_misc.so \
     $(LOCAL_PATH)/proprietary/lib/libnvodm_query.so:/system/lib/libnvodm_query.so \
-    $(LOCAL_PATH)/proprietary/lib/libnvomxilclient.so:/system/lib/libnvomxilclient.so \
     $(LOCAL_PATH)/proprietary/lib/libnvomx.so:/system/lib/libnvomx.so \
+    $(LOCAL_PATH)/proprietary/lib/libnvomxadaptor.so:/system/lib/libnvomxadaptor.so \
+    $(LOCAL_PATH)/proprietary/lib/libnvomxilclient.so:/system/lib/libnvomxilclient.so \
     $(LOCAL_PATH)/proprietary/lib/libnvos.so:/system/lib/libnvos.so \
     $(LOCAL_PATH)/proprietary/lib/libnvparser.so:/system/lib/libnvparser.so \
     $(LOCAL_PATH)/proprietary/lib/libnvrm_graphics.so:/system/lib/libnvrm_graphics.so \
@@ -114,17 +125,17 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/lib/libnvtvmr.so:/system/lib/libnvtvmr.so \
     $(LOCAL_PATH)/proprietary/lib/libnvwinsys.so:/system/lib/libnvwinsys.so \
     $(LOCAL_PATH)/proprietary/lib/libnvwsi.so:/system/lib/libnvwsi.so \
-    $(LOCAL_PATH)/proprietary/lib/libOlaBase.so:/system/lib/libOlaBase.so \
-    $(LOCAL_PATH)/proprietary/lib/libOlaEngine.so:/system/lib/libOlaEngine.so \
-    $(LOCAL_PATH)/proprietary/lib/libolaworks.so:/system/lib/libolaworks.so \
+    $(LOCAL_PATH)/proprietary/lib/libnvremoteevtmgr.so:/system/lib/libnvremoteevtmgr.so \
+    $(LOCAL_PATH)/proprietary/lib/libnvremotell.so:/system/lib/libnvremotell.so \
+    $(LOCAL_PATH)/proprietary/lib/libnvremoteprotocol.so:/system/lib/libnvremoteprotocol.so \
+    $(LOCAL_PATH)/proprietary/lib/libOlaEngineNew.so:/system/lib/libOlaEngineNew.so \
     $(LOCAL_PATH)/proprietary/lib/libpos.so:/system/lib/libpos.so \
     $(LOCAL_PATH)/proprietary/lib/libposteffect.so:/system/lib/libposteffect.so \
     $(LOCAL_PATH)/proprietary/lib/librilpos.so:/system/lib/librilpos.so \
     $(LOCAL_PATH)/proprietary/lib/libscalado.so:/system/lib/libscalado.so \
     $(LOCAL_PATH)/proprietary/lib/libsensors_mpl.so:/system/lib/libsensors_mpl.so \
     $(LOCAL_PATH)/proprietary/lib/libstagefrighthw.so:/system/lib/libstagefrighthw.so \
-    $(LOCAL_PATH)/proprietary/lib/libsupllocationprovider.so:/system/lib/libsupllocationprovider.so \
-    $(LOCAL_PATH)/proprietary/lib/libsurfaceflinger_client.so:/system/lib/libsurfaceflinger_client.so
+    $(LOCAL_PATH)/proprietary/lib/libsupllocationprovider.so:/system/lib/libsupllocationprovider.so
 
 # Firmware
 PRODUCT_COPY_FILES += \
@@ -132,19 +143,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/etc/firmware/fmc_init_1283.2.bts:system/etc/firmware/fmc_init_1283.2.bts \
     $(LOCAL_PATH)/proprietary/etc/firmware/fm_rx_init_1283.1.bts:system/etc/firmware/fm_rx_init_1283.1.bts \
     $(LOCAL_PATH)/proprietary/etc/firmware/fm_rx_init_1283.2.bts:system/etc/firmware/fm_rx_init_1283.2.bts \
+    $(LOCAL_PATH)/proprietary/etc/firmware/nvavp_aud_ucode.bin:system/etc/firmware/nvavp_aud_ucode.bin \
     $(LOCAL_PATH)/proprietary/etc/firmware/nvavp_os_0ff00000.bin:system/etc/firmware/nvavp_os_0ff00000.bin \
-    $(LOCAL_PATH)/proprietary/etc/firmware/nvavp_os_00001000.bin:system/etc/firmware/nvavp_os_00001000.bin \
-    $(LOCAL_PATH)/proprietary/etc/firmware/nvavp_os_e0000000.bin:system/etc/firmware/nvavp_os_e0000000.bin \
     $(LOCAL_PATH)/proprietary/etc/firmware/nvavp_os_eff00000.bin:system/etc/firmware/nvavp_os_eff00000.bin \
-    $(LOCAL_PATH)/proprietary/etc/firmware/nvrm_avp_0ff00000.bin:system/etc/firmware/nvrm_avp_0ff00000.bin \
-    $(LOCAL_PATH)/proprietary/etc/firmware/nvrm_avp_8e000000.bin:system/etc/firmware/nvrm_avp_8e000000.bin \
-    $(LOCAL_PATH)/proprietary/etc/firmware/nvrm_avp_9e000000.bin:system/etc/firmware/nvrm_avp_9e000000.bin \
-    $(LOCAL_PATH)/proprietary/etc/firmware/nvrm_avp_00001000.bin:system/etc/firmware/nvrm_avp_00001000.bin \
-    $(LOCAL_PATH)/proprietary/etc/firmware/nvrm_avp_be000000.bin:system/etc/firmware/nvrm_avp_be000000.bin \
-    $(LOCAL_PATH)/proprietary/etc/firmware/nvrm_avp_e0000000.bin:system/etc/firmware/nvrm_avp_e0000000.bin \
-    $(LOCAL_PATH)/proprietary/etc/firmware/nvrm_avp_eff00000.bin:system/etc/firmware/nvrm_avp_eff00000.bin \
-    $(LOCAL_PATH)/proprietary/etc/firmware/nvavp_vid_ucode.bin:/system/etc/firmware/nvavp_vid_ucode.bin \
-    $(LOCAL_PATH)/proprietary/etc/firmware/nvavp_vid_ucode_alt.bin:/system/etc/firmware/nvavp_vid_ucode_alt.bin \
+    $(LOCAL_PATH)/proprietary/etc/firmware/nvavp_vid_ucode.bin:system/etc/firmware/nvavp_vid_ucode.bin \
+    $(LOCAL_PATH)/proprietary/etc/firmware/nvavp_vid_ucode_alt.bin:system/etc/firmware/nvavp_vid_ucode_alt.bin \
     $(LOCAL_PATH)/proprietary/etc/firmware/TIInit_10.4.27.bts:system/etc/firmware/TIInit_10.4.27.bts \
     $(LOCAL_PATH)/proprietary/etc/firmware/TIInit_10.5.20.bts:system/etc/firmware/TIInit_10.5.20.bts \
     $(LOCAL_PATH)/proprietary/etc/firmware/TIInit_10.6.15.bts:system/etc/firmware/TIInit_10.6.15.bts \
